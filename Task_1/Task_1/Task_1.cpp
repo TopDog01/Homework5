e483c2bffd9bde21d4ccd883bb6f950ff7bcdf9c
Yuri Konev <crazy_clown93@mail.ru>
2025-02-07 21:58:14 +0300

    Task1

---
+++ Task_1/Task_1/Task_1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    double speed;
+    double altitude;
+
+   
+    std::cout << "Введите скорость самолета (км/ч): ";
+    std::cin >> speed;
+
+    
+    std::cout << "Введите высоту полета (метры): ";
+    std::cin >> altitude;
+
+    
+    if (speed >= 750 && speed <= 850 && altitude >= 9000 && altitude <= 9500) {
+        std::cout << "Самолет летит правильным эшелоном." << std::endl;
+    }
+    else {
+        std::cout << "Самолет не соответствует требованиям эшелона." << std::endl;
+    }
+
+    return 0;
+}
